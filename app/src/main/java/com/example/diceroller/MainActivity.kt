29af54ae9914c5a118794e4f7c5e5b2eb8d47daa
subzero <shubham8111994@gmail.com>
2023-10-07 15:11:23 +0530

    updated........

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.diceroller
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
@@ -16,10 +15,9 @@ class MainActivity : AppCompatActivity() {
 
     fun createCard(view: View) {
         val nameInput: EditText? = findViewById(R.id.enter_The_Name)
-        val name = nameInput?.text
-        val btn: Button? = findViewById(R.id.createCardButton)
+        val name = nameInput?.text.toString()
         val intent = Intent(this, GreetingActivity::class.java)
-        intent.putExtra("data", name.toString())
+        intent.putExtra("data", name)
         startActivity(intent)
     }
 }
